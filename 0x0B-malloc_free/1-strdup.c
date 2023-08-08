509d0d4d38f8f92697244b1b6f6f83575a63fdc6
ebenezerchukuigwe <ebenezerchukuigwe@gmail.com>
2023-08-08 14:22:04 -0700

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,9 +3,9 @@
 #include "main.h"
 
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ *_strdup - duplicate to new memory space location
+ *@str: char
+ *Return: 0
  */
 
 char *_strdup(char *str)
@@ -15,7 +15,6 @@ char *_strdup(char *str)
 	char *aaa;
 
 	int i, r = 0;
-
 
 	if (str == NULL)
 
@@ -27,20 +26,15 @@ char *_strdup(char *str)
 
 		i++;
 
-
 	aaa = malloc(sizeof(char) * (i + 1));
-
 
 	if (aaa == NULL)
 
 		return (NULL);
 
-
 	for (r = 0; str[r]; r++)
 
 		aaa[r] = str[r];
 
-
 	return (aaa);
-
 }
