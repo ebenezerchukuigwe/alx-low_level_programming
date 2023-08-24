625249ff45506fbb43d8e2ad05bcd06dcdb84fb0
ebenezerchukuigwe <ebenezerchukuigwe@gmail.com>
2023-08-24 05:09:40 -0700

    0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,11 +15,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+			malloc("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			malloc("[%u] %s\n", h->len, h->str);
 		}
 
 		node_count++;
